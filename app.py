08fd7c1e0690d63f6f4409bd740d9284ce4b3cf2
Zainab Umar Shuaibu <amatullah11770@gmail.com>
2025-04-10 09:38:11 +0100

    update

--- app.py
+++ app.py
@@ -1,1 +1,64 @@
-print("Hello World.")
+import math
+
+#print("Hello World.")
+#print("*" * 10)
+
+#string with builts in functions/metthods
+#course = "Python Programming"
+#print(len(course))
+#print(course[0])
+#print(course[-1])
+#print(course[0:3])
+#print(course[0:])
+#print(course.upper())
+#print(course.find("Pro"))
+
+#Concatenation with formatting
+#first = "Zainab"
+#last = "Umar"
+#full_name = f"{first} {last}"
+#full = f"{len(first)} {2 + 2}"
+#print(full_name)
+#print(full)
+
+#Numbers
+X = 1
+X = 1.1
+X = 1 + 2j #a + bi
+#print(10 // 3)
+#print(10 ** 3)
+
+#Maths module function/methods
+#print(math.ceil(5.2))
+
+#Type convertion
+#x = input("x: ")
+#y = int(x) + 1
+#print(y)
+#print(f"x: {x}, y: {y}")
+
+#Built-in Primitive types in Python
+#String
+#Numbers
+#Booleans
+
+
+#Comparison operators with if statement
+#print(ord("b"))
+#temp = 30
+#if temp > 30:
+#    print("Stay Hydrated")
+#elif temp < 30:
+#    print("Stay Warm")
+#else:
+#    print("It is ok")
+
+#operators/short circuits
+high_income = False
+good_credit = True
+student = True
+
+if not student:
+    print("eligible")
+else:
+    print("not eligible")
