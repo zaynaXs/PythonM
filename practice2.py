1546d549b51b95353286b1d42637695bc36d5160
Zainab Umar Shuaibu <amatullah11770@gmail.com>
2025-04-18 18:49:06 +0100

    update

---
+++ practice2.py
@@ -0,0 +1,11 @@
+age = 20
+#ternary oprator
+message = "Eligible" if age >= 18 else "Not Eligible"
+print(message)
+
+# and, or, not LOGICAL OPERATORS
+
+#chaining comparison operators
+age = 20
+if 18 <= age <= 65:
+    print("Eligible")
